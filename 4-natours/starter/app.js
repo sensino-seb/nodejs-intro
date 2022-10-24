06e0be956f8ccdc328c7e1cd1c86938d590000a4
sensino-seb <sebvogeltanz@gmail.com>
2022-10-24 23:17:06 +0800

    implemented the delete and patch route for the API

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,13 +6,19 @@ const port = 5000;
 app.use(express.json());
 
 // reading the data file synchronously
-const toursFile = require('path').resolve(
-  __dirname,
-  './dev-data/data/tours-simple.json'
-);
+let toursFile;
+
+try {
+  toursFile = require('path').resolve(
+    __dirname,
+    './dev-data/data/tours-simple.json'
+  );
+} catch (err) {
+  console.log(err);
+}
 
 // parse the file into a JSON object
-const tours = JSON.parse(fs.readFileSync(toursFile, 'utf-8'));
+let tours = JSON.parse(fs.readFileSync(toursFile, 'utf-8'));
 
 app.get('/api/v1/tours', (req, res) => {
   // build JSend standard for client response
@@ -25,19 +31,13 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// ':id' serves as a variable and can be read with req.params.[variable name]
 app.get('/api/v1/tours/:id', (req, res) => {
   // convert id to number and find it in tours data
   const tourId = Number(req.params.id);
-  const tour = tours.find((el) => el.id === tourId);
+  const tour = tours[tourId];
 
-  if (tourId > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  if (!tourId) {
+  if (tourId > tours.length || tourId < 0 || !tourId) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
@@ -54,9 +54,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  // add tour to tours
-  // console.log(req.body);
-
   // create new ID (in the future mongoDB will do this)
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
@@ -64,7 +61,7 @@ app.post('/api/v1/tours', (req, res) => {
   // add data to the tours array
   tours.push(newTour);
 
-  // add data to the file (to mongoDB in the future)
+  // add data to the file (mongoDB in the future)
   fs.writeFile(toursFile, JSON.stringify(tours), (err) => {
     res.status(201).json({
       status: 'success',
@@ -73,6 +70,58 @@ app.post('/api/v1/tours', (req, res) => {
       },
     });
   });
+});
+
+// update tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+  // convert id to number and find it in tours data
+  const tourId = Number(req.params.id);
+
+  if (tourId > tours.length || tourId < 0 || !tourId) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const tour = tours[tourId];
+  const updatedTour = Object.assign(tour, req.body);
+  tours[tourId] = updatedTour;
+
+  // add data to the file (mongoDB in the future)
+  fs.writeFile(toursFile, JSON.stringify(tours), (err) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  });
+});
+
+// delete tour
+app.delete('/api/v1/tours/:id', (req, res) => {
+  // convert id to number and find it in tours data
+  const tourId = Number(req.params.id);
+
+  if (tourId > tours.length || tourId < 0 || !tourId) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  // remove object from array
+  tours.splice(tourId, 1);
+
+  // add data to the file (mongoDB in the future)
+  fs.writeFile(toursFile, JSON.stringify(tours), (err) => {
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+  console.log(tours);
 });
 
 app.listen(port, (req, res) => {

4ce550204d021437c9e660c90b30db2db3848622
sensino-seb <sebvogeltanz@gmail.com>
2022-11-06 18:56:30 +0800

    connected express app with mongo db

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DB_PASSWORD
+).replace('<DB_USER>', process.env.DB_USER);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection established!'));
 
 // 3) START SERVER
 const port = process.env.PORT || 3000;

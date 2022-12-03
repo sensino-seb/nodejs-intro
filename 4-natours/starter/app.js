81e101676f1d13bf489da69394a030e52d1318a9
sensino-seb <sebvogeltanz@gmail.com>
2022-12-04 00:57:54 +0700

    added global error handler

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const errorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
@@ -18,5 +21,18 @@ app.use((req, res, next) => {
 // 2) ROUTES (middleware for specific routes)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// ROUTE handler for all routes which are not catched in tourRouter or userRouter
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Cannot find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  // if err is passed to next, express goes directly to the error handler middleware
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
+});
+
+// express automatically knows that this is error handling middleware
+app.use(errorController);
 
 module.exports = app;
